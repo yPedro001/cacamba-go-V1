94328b63f14e72fff37700d30c8d050e4db79c12
yPedro001 <pedro@exemplo.com>
2026-03-26 03:41:15 -0300

    Fix: Final map rendering stabilization (v2.1) with MapInstanceSync and global CSS import

--- src/features/map/components/MapComponent.tsx
+++ src/features/map/components/MapComponent.tsx
@@ -1,13 +1,14 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import L from 'leaflet'
-import { MapContainer } from 'react-leaflet'
+import { MapContainer, useMap } from 'react-leaflet'
 import { MapInnerContent } from './MapInnerContent'
 import { MapController } from '../hooks/useMapController'
 import { DEFAULT_CENTER, DEFAULT_ZOOM } from '../constants'
 
 /**
+ * VERSION v2.1 - Production Fix
  * Fix for Leaflet default icon issues in Next.js/Webpack
  */
 if (typeof window !== 'undefined') {
@@ -20,21 +21,36 @@ if (typeof window !== 'undefined') {
   });
 }
 
+/**
+ * Resiliently synces the Leaflet map instance with our state controller
+ */
+function MapInstanceSync({ controller }: { controller: MapController }) {
+  const map = useMap();
+  useEffect(() => {
+    if (map) {
+      console.log('[MapComponent] Map instance synced successfully (v2.1)');
+      controller.mapRef.current = map;
+      setTimeout(() => map.invalidateSize(), 500);
+    }
+    return () => {
+      console.log('[MapComponent] Map instance unmounting...');
+      controller.mapRef.current = null;
+    };
+  }, [map, controller]);
+  return null;
+}
+
 export const MapComponent = ({ controller }: { controller: MapController }) => {
   return (
-    <div className="relative h-[calc(100vh-180px)] min-h-[550px] lg:min-h-[650px] w-full z-0 overflow-hidden rounded-xl border border-border bg-slate-100 dark:bg-slate-900">
+    <div className="relative h-[calc(100vh-180px)] min-h-[550px] lg:min-h-[650px] w-full z-0 overflow-hidden rounded-xl border border-border bg-slate-100 dark:bg-slate-900 shadow-inner">
       <MapContainer
         center={DEFAULT_CENTER}
         zoom={DEFAULT_ZOOM}
         zoomControl={false}
         attributionControl={true}
-        style={{ height: '100%', width: '100%' }}
-        ref={(map) => {
-          if (map) {
-            controller.mapRef.current = map;
-          }
-        }}
+        style={{ height: '100%', width: '100%', background: '#f8fafc' }}
       >
+        <MapInstanceSync controller={controller} />
         <MapInnerContent controller={controller} />
       </MapContainer>
     </div>
