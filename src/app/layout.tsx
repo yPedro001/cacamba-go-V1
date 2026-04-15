64917f3a88db2f36ece1cf6236955b041bb98e4f
yPedro001 <pedro@exemplo.com>
2026-04-14 23:50:07 -0300

    alterar favicon para png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   keywords: ['caçamba', 'aluguel', 'gestão', 'entulho', 'locação'],
   authors: [{ name: 'CaçambaGo' }],
   robots: 'index, follow',
-  icons: { icon: '/favicon.svg' }
+  icons: { icon: '/favicon.png' }
 }
 
 export default function RootLayout({
